5c316eb8868a3f4145716f8ceb11127629a78559
Matin <matin.parsapour.iam@gmail.com>
2021-09-04 18:23:31 +0430

    create home menu

--- src/main/java/Tweeter.java
+++ src/main/java/Tweeter.java
@@ -1,4 +1,28 @@
+import util.ApplicationContext;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Tweeter {
     public static void main(String[] args) {
+        while (true) {
+            try {
+                ApplicationContext.getDemonstrateMenus().homeMenu();
+                int choice = new Scanner(System.in).nextInt();
+                if (choice == 1) {
+                    //TODO create sign up method
+                } else if (choice == 2) {
+                    //TODO create log in method
+                } else if (choice == 3) {
+                    System.out.println("Hope to see you soon");
+                    break;
+                } else {
+                    System.out.println("Choose between menu options");
+                }
+            } catch (InputMismatchException exception) {
+                System.out.println("Invalid entry");
+                System.out.println("Try again");
+            }
+        }
     }
 }
