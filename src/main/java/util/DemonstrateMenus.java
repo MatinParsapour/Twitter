2ee1f1ac94586a1c3f52d91d98bf02c147ab44c3
Matin <matin.parsapour.iam@gmail.com>
2021-09-04 18:03:23 +0430

    create DemonstrateMenus class

---
+++ src/main/java/util/DemonstrateMenus.java
@@ -0,0 +1,11 @@
+package util;
+
+public class DemonstrateMenus {
+    public void homeMenu(){
+        System.out.println("+--------------------+");
+        System.out.println("|     1.Sign up      |");
+        System.out.println("|     2.Log in       |");
+        System.out.println("|     3.Exit         |");
+        System.out.println("+--------------------+");
+    }
+}
